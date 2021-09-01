588c8a7c58a4a60c3c95026bfa41cbad2132f0a1
JasonDormier <jmdormier@gmail.com>
2021-08-31 19:02:14 -0700

    scafolding

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import Card from './components/UI/Card';
+import UserInput from './components/UserInput/UserInput'
+import Output from './components/Output/Output'
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card>
+      <UserInput/>
+      <Output/>
+    </Card>
+
   );
 }
 
---
+++ src/components/Output/Output.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+
+
+const Output = props => {
+
+    return (
+
+        <Card>
+            <span>Tip Amount/ person</span>
+            <span>$0.00</span>
+
+            <span> Total/ person</span>
+            <span>$0.00</span>
+        </Card>
+
+    )
+}
+export default Output;
